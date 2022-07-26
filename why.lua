56fb439913a23769502cf965079164f42d700ab2
akabomb-prog <69766525+akabomb-prog@users.noreply.github.com>
2022-07-26 20:40:02 +0300

    Add why.lua v2

--- why.lua
+++ why.lua
@@ -1,85 +1,62 @@
--- do you like french toast
+
 local why = {}
-local F = string.format
-local ZERO = "#{}"
+
+why.allowed = {}
+local allowed = why.allowed
+for c in ("#()[]{}char._G= "):gmatch'.' do
+    allowed[c] = true
+end
+
+why.charFunc = "([[]])[([[char]])]"
+local ZERO = "#_G"
 local ONE = "#{{}}"
-local TRUE = "[[]]==[[]]"
-local FALSE = "[[]]=={}"
-local NIL = "({})[{}]" -- never actually used but cool i guess
-local function int(n)
+
+function why.smallint(n)
     if n == 0 or n == '0' then return ZERO end
     if n == 1 or n == '1' then return ONE end
-    return F("#[[%s]]", ('.'):rep(n))
+    return "#[[" .. string.rep('.', n) .. "]]"
 end
-local function bigintstr(n)
-    local nstr = tostring(n)
-    local digits = {}
-    for i = 1, #nstr do
-        digits[#digits + 1] = int(nstr:sub(i, i))
-    end
-    return "((" .. table.concat(digits, ')..(') .. "))"
-end
-local function str2int(intstr)
-    return F("(%s+%s)", ZERO, intstr)
-end
-local function int2str(intstr)
-    return '(' .. intstr .. "..[[]])"
-end
-local function bigint(n)
-    return str2int(bigintstr(n))
-end
-local map = {}
--- add allowed characters
-for c in ("(){}[]=+.:#/typesubload' "):gmatch'.' do
-    if c == "'" then c = "\\'" end
-    map[c] = "'" .. c .. "'"
-end
-local string_byte = string.byte
-local CHAR -- filled later
-local function fromstring(str)
+
+function why.int(n)
+    n = tostring(math.floor(n))
+    if #n == 1 then return why.smallint(n) end
     local outstr = {}
-    for c in str:gmatch'.' do
-        outstr[#outstr + 1] = map[c] or F("%s(%s)", CHAR, bigint(string_byte(c)))
+    for d in n:gmatch'.' do
+        outstr[#outstr + 1] = why.smallint(d)
     end
-    return table.concat(outstr, '..')
+    return '(' .. ZERO .. "+(" .. table.concat(outstr, "..") .. "))"
 end
--- TODO: find best type and sub combinations
--- GOAL: get "string" (have: s, t)
-map.r = F("type([[]]):sub(%s,%s)", int(3), int(3))
-map.g = F("type([[]]):sub(%s,%s)", int(6), int(6))
-local inf = int2str(ONE .. '/' .. ZERO)
-map.i = F("%s:sub(%s,%s)", inf, ONE, ONE)
-map.n = F("%s:sub(%s,%s)", inf, int(2), int(2))
--- ^ "string" achieved
--- GOAL: get "char" (can't, missing 'h')
--- GOAL: get "coroutine"
-map.c = F("type(type):sub(%s,%s)", int(4), int(4))
-map.t = F("type({}):sub(%s,%s)", ONE, ONE)
--- ^ "coroutine" achieved
--- GOAL: get "create" (achieved)
--- GOAL: get "return" (achieved)
--- GOAL: get 'h' for "char"
-map.h = F("load(%s..' type('..%s..'.'..%s..'(type))')():sub(%s,%s)", fromstring("return"), fromstring("coroutine"), fromstring("create"), int(2), int(2))
-CHAR = F("load(%s..' st'..%s..'.'..%s)()", fromstring("return"), fromstring("ring"), fromstring("char"))
--- ^ "char achieved"
--- bonus characters
-map.f = F("%s:sub(%s,%s)", inf, int(3), int(3))
-map.l = F("type({}):sub(%s,%s)", int(4), int(4))
-local function program(code)
-    return "load(" .. fromstring(code) .. ")()"
+
+function why.toEvaluableString(str, level)
+    level = string.rep('=', level or 1)
+    local lb, rb = '[' .. level .. '[', ']' .. level .. ']'
+    local members = {}
+    local compound = {}
+    for c in str:gmatch'.' do
+        if allowed[c] then
+            compound[#compound + 1] = c
+        else
+            if #compound > 0 then
+                -- if we ended a compound string, append it to members
+                members[#members + 1] = lb .. table.concat(compound, '') .. rb
+                compound = {}
+            end
+            members[#members + 1] = why.charFunc .. '(' .. why.int(string.byte(c)) .. ')'
+        end
+    end
+    if #compound > 0 then
+        -- if we had a compound string, append it to members
+        members[#members + 1] = lb .. table.concat(compound, '') .. rb
+    end
+    return '(' .. table.concat(members, ")..(") .. ')'
 end
-return {
-    ZERO = ZERO,
-    ONE = ONE,
-    TRUE = TRUE,
-    FALSE = FALSE,
-    NIL = NIL,
-    map = map,
-    int = int,
-    bigintstr = bigintstr,
-    str2int = str2int,
-    int2str = int2str,
-    bigint = bigint,
-    fromstring = fromstring,
-    program = program
-}
+
+function why.getGlobal(name, lvl)
+    return "_G[" .. why.toEvaluableString(name, lvl) .. ']'
+end
+
+function why.toProgram(code)
+    return why.getGlobal("load") .. '(' .. why.toEvaluableString(code) .. ")()"
+end
+
+return why
